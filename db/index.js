71dfe4c752b09fd8f2d38125cd44914168c306d5
Hayat <aghdoubehayat@gmail.com>
2025-03-01 13:39:09 +0100

    sequelize needed a dialect

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
-
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize(process.env.PG_URI);
+dotenv.config();
 
+const sequelize = new Sequelize(process.env.PG_URI, {
+  dialect: 'postgres',  
+  logging: false,       
+
+});
 export default sequelize;
