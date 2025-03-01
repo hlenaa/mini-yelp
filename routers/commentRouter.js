f3bd47b66a8cb842f98ac63a1923769fc482d9f9
Hayat <aghdoubehayat@gmail.com>
2025-03-01 12:29:54 +0100

    added getComments

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { addComment } from '../controllers/commentController.js';
+import { addComment , getComments } from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.post('/comments', addComment);
+router.get('/comments/:restaurantId', getComments);
 
 export default router;
